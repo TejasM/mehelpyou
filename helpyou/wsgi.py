74ff0e6a2da52a231b3fc410f127ec3c502e7750
TejasM <tejasmehta0@gmail.com>
2013-09-10 21:20:26 -0400

    Wsgi

--- helpyou/wsgi.py
+++ helpyou/wsgi.py
@@ -15,11 +15,18 @@ framework.
 """
 import os
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "helpyou.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helpyou.settings")
+# Add the site-packages of the chosen virtualenv to work with
+import site
+import sys
+
+site.addsitedir('/home/mehelp5/dVLU4yf1mr47FdacwqjQYw/help_env/lib/python2.6/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/mehelp5/dVLU4yf1mr47FdacwqjQYw/')
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/mehelp5/dVLU4yf1mr47FdacwqjQYw/help_env/binactivate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
