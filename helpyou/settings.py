cf25e47ce42c9cfe05c5d262298832afa4e0f221
TejasM <tejasmehta0@gmail.com>
2013-09-13 21:41:04 -0400

    South

--- helpyou/settings.py
+++ helpyou/settings.py
@@ -148,7 +148,7 @@ INSTALLED_APPS = (
     'helpyou.request',
     'helpyou.response',
     'helpyou.userprofile',
-    # 'south',
+    'south',
     'social_auth',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable admin documentation:
