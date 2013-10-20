adfe0b267d3e1df49cc657d4cb7cd82ceb390d7d
TejasM <tejasmehta0@gmail.com>
2013-10-20 18:38:52 -0400

    Update email

--- helpyou/settings.py
+++ helpyou/settings.py
@@ -249,7 +249,7 @@ BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 
 CELERY_IMPORTS = ("tasks",)
 # smtp settings for email
-EMAIL_HOST = 'vps8073.inmotionhosting.com'
+EMAIL_HOST = 'localhost'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'info@mehelpyou.com'
 EMAIL_HOST_PASSWORD = 'tejas'
