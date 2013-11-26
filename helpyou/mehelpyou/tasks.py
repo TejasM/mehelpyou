a0ccd9cd327cd4b504187c1fbafdabecb0c1469f
TejasM <tejasmehta0@gmail.com>
2013-11-26 10:16:38 -0500

    Message

--- helpyou/mehelpyou/tasks.py
+++ helpyou/mehelpyou/tasks.py
@@ -19,12 +19,12 @@ def email_tasks():
     send_all()
 
 
-@periodic_task(run_every=timedelta(minutes=1))
+@periodic_task(run_every=timedelta(minutes=60))
 def weekly_digest():
     users = User.objects.all()
     timenow = timezone.now()
     for user in users:
-        if user.email == "tejasmehta0@gmail.com":
+        if user.email:
             try:
                 profile = user.user_profile.get()
                 connections = profile.connections.all()
@@ -57,14 +57,16 @@ def write_weekly_email(user, connections_requests, your_requests, negotiations, 
     message += "<p>Your Connections' Pending Requests:"
     for request in connections_requests:
         message += "<br><a href='www.mehelpyou.com/request/view/" + str(request.id) + "'>" + request.title + "</a> from " + request.user.username
-    message += "</p><br><p>"
-    for request, responses in your_requests.iteritems():
-        message += "<br><p>For your request: <a href='www.mehelpyou.com/request/view/" + str(request.id) + "'>" + request.title + "</a> you have the following responses:"
-        for response in responses:
-            message += "<br><p style='text-indent: 5em;'> <a href='www.mehelpyou.com/response/view/" + str(response.id) + "'>" + response.preview + "</a></p>"
-    message += "</p><br><p>"
-    for negotiation in negotiations:
-        message += "<br><p>Your <a href='www.mehelpyou.com/response/view/" + str(negotiation.response.id) + "'>response</a> for request " + negotiation.request.title + " has a negotiation."
+    if your_requests.iteritems():
+        message += "</p><br><p>"
+        for request, responses in your_requests.iteritems():
+            message += "<br><p>For your request: <a href='www.mehelpyou.com/request/view/" + str(request.id) + "'>" + request.title + "</a> you have the following responses:"
+            for response in responses:
+                message += "<br><p style='text-indent: 5em;'> <a href='www.mehelpyou.com/response/view/" + str(response.id) + "'>" + response.preview + "</a></p>"
+    if negotiations:
+        message += "</p><br><p>"
+        for negotiation in negotiations:
+            message += "<br><p>Your <a href='www.mehelpyou.com/response/view/" + str(negotiation.response.id) + "'>response</a> for request " + negotiation.request.title + " has a negotiation."
     message += "<br>This week you have earned " + str(points_earned) + " points by helping your friends out"
     message += settings.NOTE
     send_html_mail('MeHelpYou Digest', "", message, 'info@mehelpyou.com', [user.email], fail_silently=True)
