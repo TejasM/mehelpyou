86660d98efdf17fe3dcad294881721974982e81f
TejasM <tejasmehta0@gmail.com>
2013-11-26 10:02:42 -0500

    Message

--- helpyou/mehelpyou/tasks.py
+++ helpyou/mehelpyou/tasks.py
@@ -14,9 +14,9 @@ from helpyou.response.models import Response
 
 # this will run every 1 seconds
 # send all emails in the mailer queue
-@periodic_task(run_every=timedelta(seconds=1))
-def email_tasks():
-    send_all()
+# @periodic_task(run_every=timedelta(seconds=1))
+# def email_tasks():
+#     send_all()
 
 
 @periodic_task(run_every=timedelta(minutes=1))
