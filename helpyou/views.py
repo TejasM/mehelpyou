0760d406e5b6b709df31daf574fdb9b0fd5a105c
Tejas <tejasmehta0@gmail.com>
2014-02-14 17:48:59 -0500

    Fixed login and sign up

--- helpyou/views.py
+++ helpyou/views.py
@@ -21,21 +21,28 @@ def index(request):
                                            last_name=form.data["last_name"])
                 user.set_password(form.data["password"])
                 user.save()
-                messages.success(request, 'Account created, please proceed by logging in.')
-                return HttpResponseRedirect(reverse('user:feed'))
+                user = authenticate(username=request.POST.get('email', ''), password=request.POST.get('password', ''))
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                    else:
+                        return redirect('/')
+                else:
+                    return redirect('/')
+                return redirect(reverse('user:feed'))
         elif request.POST['type'] == 'login':
             user = authenticate(username=request.POST.get('username', ''), password=request.POST.get('password', ''))
             if user is not None:
                 if user.is_active:
                     login(request, user)
                 else:
-                    messages.success(request, 'Your account needs password reset, please follow link sent to your email')
-                    render(request, "base-home.html", {'form': SignupForm()})
+                    return redirect('/')
                 return redirect(reverse('user:feed'))
             else:
                 try:
                     User.objects.get(username=request.POST.get('username', ''))
                 except User.DoesNotExist as _:
-                    return render(request, "base-home.html", {'username': True, 'form': SignupForm()})
-                return render(request, "base.html", {'password': True, 'form': SignupForm()})
+                    messages.error(request, 'No such user')
+                    return redirect('/')
+                return redirect('/')
     return render(request, "base-home.html", {'form': SignupForm()})
