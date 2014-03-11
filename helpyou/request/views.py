54ffcf64732d64a30ab970a61cf8bf9aa1776f37
Tejas <tejasmehta0@gmail.com>
2014-03-10 21:17:28 -0400

    better fix

--- helpyou/request/views.py
+++ helpyou/request/views.py
@@ -95,12 +95,15 @@ def view_id(request, id_request):
     messages_for_request = Message.objects.filter(request=request_your)
     users_id_list = set(messages_for_request.values_list('message_from_user__id', flat=True))
     users_id_list = list(map(lambda x: int(x), users_id_list))
+    first_id = None
+    if users_id_list:
+        first_id = users_id_list[0]
     users_first_list = set(messages_for_request.values_list('message_from_user__first_name', flat=True))
     users_last_list = set(messages_for_request.values_list('message_from_user__last_name', flat=True))
     return render(request, "request/view_your_request.html",
                   {'request_your': request_your, "responses": responses, "have_responded": False,
                    'messages_list': messages_for_request, 'users_id_list': users_id_list,
-                   'users_first_list': users_first_list, 'users_last_list': users_last_list})
+                   'users_first_list': users_first_list, 'users_last_list': users_last_list, 'first_id': first_id})
 
 
 @login_required
