679b5de08f1f04899940f856dab5a0aa68a730d5
Tejas <tejasmehta0@gmail.com>
2014-03-17 20:36:55 -0400

    Change settings

--- helpyou/settings.py
+++ helpyou/settings.py
@@ -19,24 +19,24 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'
-#         'NAME': 'mehelp5_help', # Or path to database file if using sqlite3.
-#         # The following settings are not used with sqlite3:
-#         'USER': 'mehelp5_tejas',
-#         'PASSWORD': 'tejas',
-#         'HOST': '127.0.0.1', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '', # Set to empty string for default.
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'database'), # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'
+        'NAME': 'mehelp5_help', # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'mehelp5_tejas',
+        'PASSWORD': 'tejas',
+        'HOST': '127.0.0.1', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '', # Set to empty string for default.
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': os.path.join(SITE_ROOT, 'database'), # Or path to database file if using sqlite3.
+#     }
+# }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -257,14 +257,14 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-#
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#
-# # Allow all host headers
-ALLOWED_HOSTS = ['*']
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
+# #
+# # # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# #
+# # # Allow all host headers
+# ALLOWED_HOSTS = ['*']
 #
 STRIPE_PUBLIC_KEY = "pk_test_HlXsmOAZkKNhrPmUQ7w8Iumi"
 STRIPE_SECRET_KEY = "sk_test_wQyxu2ZMVuK9ynbiFQTc2FOz"
