470cc7de921c8b3c03106df0202c2821b3e55dce
Tejas <tejasmehta0@gmail.com>
2014-05-23 21:10:23 -0400

    few fixes

--- helpyou/settings.py
+++ helpyou/settings.py
@@ -42,7 +42,8 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['www.mehelpyou.com', 'mehelpyou.com', 'www.vps8073.inmotionhosting.com', 'vps8073.inmotionhosting.com']
+ALLOWED_HOSTS = ['www.mehelpyou.com', 'mehelpyou.com', 'www.vps8073.inmotionhosting.com', 'vps8073.inmotionhosting.com',
+                 'mail.mehelpyou.com', '70.39.151.121']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
