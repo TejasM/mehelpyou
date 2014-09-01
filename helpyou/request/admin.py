076f79a874c431464c60d7a1bc35812665a4e00b
Tejas Mehta <tejasmehta0@gmail.com>
2014-09-01 09:14:26 -0400

    Feed also

--- helpyou/request/admin.py
+++ helpyou/request/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from helpyou.request.models import Request
+from helpyou.userprofile.models import Feed
 
 __author__ = 'tmehta'
-
+admin.site.register(Feed)
 admin.site.register(Request)
