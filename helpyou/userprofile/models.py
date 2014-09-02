3fd56f5d8df70f602619b59d8ed87b9053c96a7e
Tejas Mehta <tejasmehta0@gmail.com>
2014-09-02 11:41:54 -0400

    blank request

--- helpyou/userprofile/models.py
+++ helpyou/userprofile/models.py
@@ -73,8 +73,8 @@ class Feed(models.Model):
     avatar_link = models.CharField(max_length=1000, default="/avatars/default-avatar.png")
     users = models.ManyToManyField(User)
     time = models.DateTimeField(auto_now_add=True)
-    request = models.ForeignKey(Request, default=None, null=True)
-    response = models.ForeignKey(Response, default=None, null=True)
+    request = models.ForeignKey(Request, default=None, null=True, blank=True)
+    response = models.ForeignKey(Response, default=None, null=True, blank=True)
 
 
 class Message(models.Model):
