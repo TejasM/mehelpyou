110011436c53e5d0fc8058861ce1f83b1f0c32e0
Tejas Mehta <tejasmehta0@gmail.com>
2014-09-03 08:36:51 -0400

    ADd social

--- helpyou/settings.py
+++ helpyou/settings.py
@@ -128,6 +128,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'tz_detect.middleware.TimezoneMiddleware',
+    'social_auth.middleware.SocialAuthExceptionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
