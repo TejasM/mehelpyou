657a2b7e49637079ef51071409da694e6c4d08d2
Tejas Mehta <tejasmehta0@gmail.com>
2014-09-04 17:23:38 -0400

    Remove fromfront

--- helpyou/views.py
+++ helpyou/views.py
@@ -53,7 +53,7 @@ def index(request):
                 messages.error(request, 'No Such User')
                 return redirect('/')
     featured_requests = []
-    featured_requests_id = [7, 2, 3, 8, 4, 5, 6, 9, 10, 11, 12, 13]
+    featured_requests_id = [7, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13]
     try:
         for r_id in featured_requests_id:
             featured_requests.append(Request.objects.get(pk=r_id))
