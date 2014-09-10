f41663351a3ffccf45d6f44cca9b58818911cee5
Tejas Mehta <tejasmehta0@gmail.com>
2014-09-10 16:49:49 -0400

    Fix message

--- helpyou/userprofile/views.py
+++ helpyou/userprofile/views.py
@@ -461,7 +461,7 @@ def feed(request):
         feeds = feeds.filter(request__city__iregex=r'(' + '|'.join(city) + ')')
     if 'page' in data:
         del data['page']
-    feeds = feeds.order_by('-request__commission_end')
+    feeds = feeds.order_by('-request__commission_end', 'id')
     form = requests_inner.form
     paginator = Paginator(feeds, 5)
     page = request.GET.get('page')
